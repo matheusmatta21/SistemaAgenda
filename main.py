acf7f8be9664ad70c2b3ef5c57301d7c20aee5b5
matheusmatta21 <138629402+matheusmatta21@users.noreply.github.com>
2023-12-15 14:45:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,63 @@
 from tkinter import *
 from tkinter.ttk import Combobox
-from tkinter import messagebox 
+from tkinter import messagebox
 
-def escreverDados(): #sem validação
-        nome = inputNome.get()
-        numero = int(inputNumero.get())
-        email = inputEmail.get() 
-        preferencial = comboboxPreferencial.get()
-        arquivo = open("contatos.txt", "a", encoding="utf-8")
-        arquivo.write(f"{nome},{numero},{email},{preferencial}\n")
-        arquivo.close()
-    
 
-def validarDados(): ##com validacao incompleta
-        booleano = True
-        nome = inputNome.get()
-        numero = inputNumero.get()
-        email = inputEmail.get()
-        preferencial = comboboxPreferencial.get()
-        if nome == '' or nome == ' ':
-            messagebox.showerror(title="Erro", message="Digite um nome!")
-            booleano = False
-        elif numero == "" or numero == " " or numero.isnumeric() == False:
-            messagebox.showerror(title="Erro", message="Digite um número!")
-            booleano = False
-        elif email == '' or email == " ":
-            messagebox.showerror(title="Erro", message="Digite um email!")
-            booleano = False
-        elif "@" not in email and "." not in email:
-            messagebox.showerror(title="Erro", message="Insira a formatação certa do e-mail!")
-            booleano = False
-        elif preferencial == "" or preferencial == " ":
-            messagebox.showerror(title="Erro", message="Selecione se você faz parte do grupo preferencial!")
-            booleano = False
-        if booleano == True:
-            escreverDados()
-        else:
-            pass
-       
-     
+def escreverDados():
+    nome = inputNome.get()
+    numero = int(inputNumero.get())
+    email = inputEmail.get()
+    preferencial = comboboxPreferencial.get()
+    arquivo = open("contatos.txt", "a", encoding="utf-8")
+    arquivo.write(f"{nome},{numero},{email},{preferencial}\n")
+    arquivo.close()
+
+
+def validarDados():
+    booleano = True
+    nome = inputNome.get()
+    numero = inputNumero.get()
+    email = inputEmail.get()
+    preferencial = comboboxPreferencial.get()
+    if nome == "" or nome == " ":
+        messagebox.showerror(title="Erro", message="Digite um nome!")
+        booleano = False
+    elif numero == "" or numero == " " or numero.isnumeric() == False:
+        messagebox.showerror(title="Erro", message="Digite um número!")
+        booleano = False
+    elif email == "" or email == " ":
+        messagebox.showerror(title="Erro", message="Digite um email!")
+        booleano = False
+    elif "@" not in email and "." not in email:
+        messagebox.showerror(
+            title="Erro", message="Insira a formatação certa do e-mail!"
+        )
+        booleano = False
+    elif preferencial == "" or preferencial == " ":
+        messagebox.showerror(
+            title="Erro", message="Selecione se você faz parte do grupo preferencial!"
+        )
+        booleano = False
+    if booleano == True:
+        escreverDados()
+    else:
+        pass
+
 
 def lerDados():
-    windowTwo = Tk() #passivel de erro
+    windowTwo = Tk()
     windowTwo.title("Lista de contatos")
     windowTwo.geometry("1000x500")
 
     arquivo = open("contatos.txt", "r", encoding="utf-8")
     for linha in arquivo.readlines():
         contato = linha.split(",")
-        labelContatos = Label(windowTwo, text=f"contato:{contato[0]},{contato[1]},{contato[2]},{contato[3]}")
+        labelContatos = Label(
+            windowTwo,
+            text=f"contato:{contato[0]},{contato[1]},{contato[2]},{contato[3]}",
+        )
         labelContatos.pack()
+
 
 def limparFormulario():
     inputNome.delete(0, END)
@@ -58,49 +66,38 @@ def limparFormulario():
     comboboxPreferencial.delete(0, END)
 
 
-
-
 window = Tk()
 window.title("Sistema de Agenda de Contatos")
 window.geometry("600x300")
-
-#nome
 
 lblNome = Label(window, text="Nome:")
 lblNome.pack()
 inputNome = Entry(window, text="Nome", bd=2)
 inputNome.pack()
 
-#numero
-
 lblNumero = Label(window, text="Número de telefone:")
 lblNumero.pack()
 inputNumero = Entry(window, bd=2)
 inputNumero.pack()
-
-#email
 
 lblEmail = Label(window, text="Endereço de e-mail:")
 lblEmail.pack()
 inputEmail = Entry(window, text="Email", bd=2)
 inputEmail.pack()
 
-#preferencial
-
 valores = ("Sim", "Nâo")
-labelpreferencial = Label(window,text="Preferencial:")
+labelpreferencial = Label(window, text="Preferencial:")
 labelpreferencial.pack()
 comboboxPreferencial = Combobox(window, values=valores)
 comboboxPreferencial.pack()
 
-#Botões de enviar e limpar e ver contatos
-btn = Button(window,text="Enviar", command=validarDados)
+btn = Button(window, text="Enviar", command=validarDados)
 btn.pack()
 
-btn2 = Button(window,text="Limpar", command=limparFormulario)
+btn2 = Button(window, text="Limpar", command=limparFormulario)
 btn2.pack()
 
-btn3 = Button(window,text="Ver contatos",command=lerDados)
+btn3 = Button(window, text="Ver contatos", command=lerDados)
 btn3.pack()
 
 window.mainloop()
